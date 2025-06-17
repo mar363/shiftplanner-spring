e3bf3103795a848cd7ec51470e6c6950264e8c28
Cristina Zaharia <ncristinanedea@gmail.com>
2025-06-17 23:44:11 +0300

    Initial commit: Spring Boot project

---
+++ src/main/java/com/demo/shiftplanner/shift_planner/ShiftplannerSpringApplication.java
@@ -0,0 +1,13 @@
+package com.demo.shiftplanner.shift_planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiftplannerSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShiftplannerSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/shiftplanner/shift_planner/ShiftplannerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.shiftplanner.shift_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiftplannerSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
